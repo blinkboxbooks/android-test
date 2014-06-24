6d68e383950a2887089e79a808f232ce3615ed1b
alexjo <alexjo@blinkbox.com>
2014-06-24 16:02:27 +0100

    wip, dynamic config for environments

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -2,6 +2,7 @@ Given(/^the anonymous library screen is displayed$/) do
 	if element_exists("* marked:'Sign in or register'")
 		@anonymous_library_page = page(WelcomePage).try_application_out
 	else
+		puts "already signed in!"
 		@anonymous_library_page = page(AnonymousLibraryPage).await
 	end
 	page(AnonymousLibraryPage).await.expect be_current_page	
@@ -14,5 +15,5 @@ When(/^I sign in$/) do
 	page(SignInPage).submit_sign_in_details(username,password)
 end
 Then(/^I should be on the user library screen$/) do
-	@user_library_page = page(UserLibraryPage).await
+	@user_library_page = page(UserLibraryPage).await.expect be_current_page
 end
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,10 +1,58 @@
 require 'yaml'
+require 'httparty'
 require 'calabash-android/cucumber'
 Before do
-	$dunit ||= false  # have to define a variable before we can reference its value
-	return $dunit if $dunit                  # bail if $dunit TRUE
+	$dunit ||= false
+	return $dunit if $dunit              
 	#load configuration files
 	@conf = YAML.load_file("config/project_settings")
 	@testdata = YAML.load_file("config/test_settings")
-	$dunit = true                            # don't do it again.
+
+	@environment = @testdata['test']['environment']
+	if @environment.nil?
+		puts "Alert - no configured environment"
+		exit
+	end	
+	@auth = @testdata['environment'][@environment]['url']['auth']
+	@host = @testdata['environment'][@environment]['qa']['host']
+	$dunit = true                           
+
 end
+module KnowAboutOauthRequests
+	def authenticate(opt = {})
+		auth_user = Auth.new(opt)
+		@access_token = auth_user.access_token
+	end
+	def request_headers(header = {})
+		@request_headers ||= {
+			"Content-type" => "application/vnd.blinkboxbooks.data.v1+json"
+		}
+		if @access_token
+			@request_headers["Authorization"] = "Bearer #{@access_token}"
+		else
+			@request_headers.delete("Authorization")
+		end
+		@request_headers.merge!(header)
+		@request_headers
+	end
+
+	class Auth
+
+		include HTTParty
+		base_uri "#{@auth}"
+
+		def initialize( option = {} )
+			@response = self.class.post('/oauth2/token', :body => option)
+		end
+		def access_token
+			@response["access_token"]
+		end
+
+	end
+end
+module LibraryService
+	def add_sample_book( host, body = {}, headers = {} )
+		@response = HTTParty.post(host, :body => body.to_json , :headers => headers )
+	end
+end
+World(KnowAboutOauthRequests, LibraryService)
