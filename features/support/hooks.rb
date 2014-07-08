be4e1aedc9ec4d94d44d6b62ebe0f724f0cbc038
alexjo <alexjo@blinkbox.com>
2014-07-08 17:02:45 +0100

    readded device logging

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,10 +1,10 @@
 Before do | scenario |
   if defined?(DeviceLogging)
-  #  dlog_start
+    dlog_start
   end
 end
 After do | scenario |
   if defined?(DeviceLogging)
-  #  dlog_stop
+    dlog_stop
   end
 end
