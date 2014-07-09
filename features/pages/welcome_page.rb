1c101fd74b3505835841c470127bfb3a17be92ef
alexjo <alexjo@blinkbox.com>
2014-07-09 16:59:48 +0100

    more suggested changes

--- features/pages/welcome_page.rb
+++ features/pages/welcome_page.rb
@@ -3,10 +3,6 @@ class WelcomePage < Page
 	element :try_it_out_button, "BBBButton marked:'Try it out'"
 
 	def goto_library
-		if try_it_out_button.exists?
 			touch(try_it_out_button.selector)
-		else
-			logger.alert "Element missing #{try_it_out_button.el}"
-		end
 	end
 end
