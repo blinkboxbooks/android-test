3130565eb1df39a82b914b294896ea3d2c3587bb
alexjo <alexjo@blinkbox.com>
2014-07-31 10:07:17 +0100

    fixing bad indentation in modules

--- features/support/modules/logged_formatter.rb
+++ features/support/modules/logged_formatter.rb
@@ -3,11 +3,11 @@ require 'cucumber/formatter/io'
 require 'cucumber/formatter/html'
 
 class LoggedFormatter < Cucumber::Formatter::Html
-	include UtilityObjects
-	def initialize(step_mother, path_or_io, options)
-		super(step_mother,File.open(conf_data['project']['log_html_path'],"w+"),options)
-	end
-	def before_steps(steps)
-		  @builder << '<ol style="display:none">'
-	end
+  include UtilityObjects
+  def initialize(step_mother, path_or_io, options)
+    super(step_mother,File.open(conf_data['project']['log_html_path'],"w+"),options)
+  end
+  def before_steps(steps)
+    @builder << '<ol style="display:none">'
+  end
 end
