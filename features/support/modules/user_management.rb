40d8f9dc775b5819afa9a7bade9f797ca8993c20
alexjo <alexjo@blinkbox.com>
2014-08-04 11:33:55 +0100

    refactored

--- features/support/modules/user_management.rb
+++ features/support/modules/user_management.rb
@@ -2,7 +2,9 @@ require 'blinkbox/user'
 include Blinkbox
 module UserManagement
   def current_user(params = {})
-    @_current_user = User.new(:grant_type => "password",:username => test_data['users']['existing']['emailaddress'], 
-                              :password => test_data['users']['existing']['password'])
+    username = params[:username] ? params[:username] : test_data['users']['existing']['emailaddress']
+    password = params[:password] ? params[:password] : test_data['users']['existing']['password'] 
+    @_current_user = User.new(:grant_type => "password",:username => username, 
+                              :password => password)
   end
 end
