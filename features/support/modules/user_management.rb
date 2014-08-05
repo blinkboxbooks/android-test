81b6d77888948f340f59e4b5c7db8b4347a51adc
alexjo <alexjo@blinkbox.com>
2014-08-05 08:23:54 +0100

    caching

--- features/support/modules/user_management.rb
+++ features/support/modules/user_management.rb
@@ -4,7 +4,7 @@ module UserManagement
   def current_user(params = {})
     username = params[:username] ? params[:username] : test_data['users']['existing']['emailaddress']
     password = params[:password] ? params[:password] : test_data['users']['existing']['password'] 
-    @_current_user = User.new(:grant_type => "password", :username => username, 
+    @_current_user ||= User.new(:grant_type => "password", :username => username, 
                               :password => password)
   end
 end
