fc9225915a65c1f4e2039371266c68c7f82624c4
alexjo <alexjo@blinkbox.com>
2014-09-22 09:08:09 +0100

    Update vagrant-provision.sh

--- android-vm/vagrant-provision.sh
+++ android-vm/vagrant-provision.sh
@@ -3,6 +3,7 @@ echo "Provisioning vagrant box"
 #this takes care of the proxy not resolving artifactory correctly
 cd /android-test
 sed 's/artifactory.blinkbox.local/192.168.16.183/g' Gemfile
+sed 's/mobile-build.mobcastdev.local/192.168.100.14/g' config/project_settings.yml
 #This gets around an error caused by bundler `require': no such file to load -- mkmf (LoadError)`
 sudo apt-get install ruby1.8-dev -y
 sudo apt-get install build-essential -y
