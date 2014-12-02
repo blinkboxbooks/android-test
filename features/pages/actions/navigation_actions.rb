d8a2664769ac565e0c255d5308298a9c12f24aa4
Sher Singh Bhachu <sherb@blinkbox.com>
2014-12-02 13:53:41 +0000

    no message

---
+++ features/pages/actions/navigation_actions.rb
@@ -0,0 +1,26 @@
+module PageObjectModel
+  module NavigationActions
+    require 'calabash-android/operations'
+
+    def go_back_to_my_library_page_from_signin_page
+      sign_in_page.await
+      navigate_back_until_my_library_page_present
+    end
+
+    def navigate_back_until_my_library_page_present
+      navigate_back_to_user_library("My Library")
+    end
+
+    def navigate_back_until_user_library_reading_page_present
+      navigate_back_to_user_library("Reading")
+    end
+
+    def navigate_back_to_user_library(text)
+      wait_poll(until_exists: "* id:'sliding_tabs' TextView {text BEGINSWITH '#{text}'} isSelected:true", timeout: 10) do
+        press_back_button
+      end
+    end
+  end
+end
+
+World(PageObjectModel::NavigationActions)
