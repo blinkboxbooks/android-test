ceaf88ae53601ebbfceca0d7f75bcbc9c15f1eae
Sher Singh Bhachu <sherb@blinkbox.com>
2014-12-18 12:23:07 +0000

    [0] - Register steps

--- features/step_definitions/register_steps.rb
+++ features/step_definitions/register_steps.rb
@@ -4,7 +4,6 @@ Given(/^I create a new user with a credit card$/) do
   puts @password
   @name_on_card = api_helper.add_credit_card
 end
-
 Given(/^I register and sign in as a new user$/) do
   enter_app_as_newly_registered_user
 end
