04454ebba9cd4aa590e9123ba25068ed3172923b
Jagdeep Kalsi <jagdeep@blinkbox.com>
2014-12-18 11:58:28 +0000

    Merge

--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -40,6 +40,11 @@ And /^I submit registration details by (accepting|not accepting) terms and condi
   submit_registration_details
 end
 
+When /^I enter email address already registered to BlinkboxBooks$/ do
+  @email_address, @first_name, @last_name = enter_personal_details(test_data['emails']['happypath_user'])
+  enter_password(test_data['passwords']['valid_password'])
+end
+
 When /^I try to sign in with an unregistered email address$/ do
   submit_sign_in_details(test_data['emails']['dummy_email'], test_data['passwords']['valid_password'])
 end
@@ -51,3 +56,54 @@ end
 Then /^sign in is not successful$/ do
   expect_page(sign_in_page)
 end
+
+Then /^registration is not successful$/ do
+  expect_page(register_page)
+end
+
+Then /^email already registered message is displayed$/ do
+  expect(register_page).to have_email_already_registered_message
+end
+
+And /^please accept T&C condition message is displayed$/ do
+  expect(register_page).to have_accept_terms_and_condition_message
+end
+
+And /^password don't match message is displayed$/ do
+  expect(register_page).to have_password_dont_match_message
+end
+
+And /^the invalid password message is displayed$/ do
+  expect(register_page).to have_password_invalid_message
+end
+
+And /^the invalid clubcard message is displayed$/ do
+  expect(register_page).to have_invalid_clubcard_message
+end
+
+And /^sign in with this email button is displayed$/ do
+  expect(register_page.sign_in_with_this_email_button).to exist
+end
+
+And /^I click on button to sign in with already registered email$/ do
+  register_page.click_on_sign_in_with_this_email_button
+end
+
+When /^I enter valid registration details$/ do
+  @email_address, @first_name, @last_name = enter_personal_details
+  enter_password(test_data['passwords']['valid_password'])
+end
+
+And /^type passwords that are not matching$/ do
+  register_page.enter_not_matching_password
+end
+
+And /^type passwords that are less than 6 characters$/ do
+  enter_password("test1")
+end
+
+
+
+
+
+
