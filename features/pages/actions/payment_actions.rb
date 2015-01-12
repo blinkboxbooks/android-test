e698d5932937dc4666f1b4df915e35236a167460
Ankur Gamit <ankurg@blinkbox.com>
2015-01-12 11:00:31 +0000

    Small change per Sher's comments

--- features/pages/actions/payment_actions.rb
+++ features/pages/actions/payment_actions.rb
@@ -23,7 +23,7 @@ module PageObjectModel
     def pay_with_a_new_card(card_type, card_details_storage)
       choose_to_purchase_with_new_card
       wait_for{ expect(shop_page).to have_enter_your_card_details_popup }
-      choose_not_save_card if card_details_storage.include?('not to')
+      choose_not_save_card if card_details_storage.include?('not')
       enter_card_details(card_type)
       shop_page.enter_card_details_popup.next_button.scroll_to
       shop_page.enter_card_details_popup.next_button.touch
